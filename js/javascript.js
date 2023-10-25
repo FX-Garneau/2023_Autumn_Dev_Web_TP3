abd67fa9682a99549cac967d53371290a5d93209
Maxime Foisy <mabea022@edu.cegepgarneau.ca>
2023-10-25 14:24:39 -0400

    Initialisation

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+/**
+ *  Fichier principal javascript
+ */
+"use strict";
+/*global ajouterALaPage*/ 
+
+/*************
+    Cette fonction est rattachée à l'événement "Load". 
+    C'est la première fonction qui va s'executer lorsque 
+    la page sera entièrement chargée.
+**************/
+ function initialisation() {
+   
+
+}
+
+
+addEventListener("load", initialisation, false);
