e3593a48e05fe694dc9ddedcd4cfa5f91c4c0e5a
Thierry Durand <thdur001@edu.cegepgarneau.ca>
2023-10-30 09:03:09 -0400

    Ajuster fonction afficherCards et afficherRetro

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,9 @@
  *  Fichier principal javascript
  */
 "use strict";
+const IDTOASTAFFICHER ="div";
+const TITRETOAST ="Bonne réponse!";
+
 /*global ajouterALaPage DATA_QUIZ*/ 
 
 /*************
@@ -39,16 +42,11 @@ partir des paramètres pour personnaliser l’image, le titre et la description.
 function creerCards(pImage, pTitre, pDescription){
     let elementHTMLPourInsererLaCards = document.getElementById("modules");
 
-    let nouvElementDivRow = document.createElement("div");
-    nouvElementDivRow.classList.add("row");
-    nouvElementDivRow.classList.add("mb-5");
-    elementHTMLPourInsererLaCards.appendChild(nouvElementDivRow);
-
     let nouvElementDivCol = document.createElement("div");
-    nouvElementDivCol.classList.add("col-lg-6");
+    nouvElementDivCol.classList.add("col-lg-4");
     nouvElementDivCol.classList.add("col-md-12");
     nouvElementDivCol.classList.add("mb-3");
-    nouvElementDivRow.appendChild(nouvElementDivCol);
+    elementHTMLPourInsererLaCards.appendChild(nouvElementDivCol);
 
     let nouvElementDivCard = document.createElement("div");
     nouvElementDivCard.classList.add("card");
@@ -87,9 +85,12 @@ paramètres passés.
  * @param {*} pContenu Description du toast selon l'endroit où il est utilisé
  * @param {*} pTemps Temps en millisecondes pour avoir réalisé le questionnaire
  */
-function afficherToasts (pId, pTitre, pContenu, pTemps)
+function afficherToasts (pId, pTitre, pContenu)
 {
+
+    //vider le toast en premier avec .innertHTML ="";
     let elementHtmlPourInsererToast = document.getElementById(pId);
+
     let nouvelleDivToast = document.createElement("div");
     nouvelleDivToast.classList.add("toast");
     elementHtmlPourInsererToast.appendChild(nouvelleDivToast);
@@ -122,4 +123,21 @@ function afficherToasts (pId, pTitre, pContenu, pTemps)
     nouvelleDivToastBody.appendChild(nouvelElementPTemps);
 }
 
+/**
+ * Va chercher la rétroaction de la réponse (bonne ou
+mauvaise) et l’affiche dans un toast si le paramètre est true
+ * @param {boolean} pEstRetroToasts Retour de la réponse
+ * @param {*} idToast Id de l'Élément HTML qui doit afficher le toast
+ */
+function afficherRetroaction(pEstRetroToasts, idToast)
+{
+    if(pEstRetroToasts)
+    {
+      afficherToasts(idToast,TITRETOAST,DATA_QUIZ.banque_questions.retroactionPositive);
+    }
+    else
+    {
+        afficherToasts(idToast,TITRETOAST,DATA_QUIZ.banque_questions.retroactionNegative);
+    }
+}
 addEventListener("load", initialisation, false);
